18a23b92c70c4b307233b609be8316051a4577a2
soumya nayak <soumyanayak.raju@gmail.com>
2024-02-05 11:02:45 +0530

    modified:   routes/user.js

--- routes/user.js
+++ routes/user.js
@@ -2,26 +2,20 @@ import express from "express";
 import passport from "passport";
 import { logout, myProfile } from "../controllers/user.js";
 import { isAuthenticated } from "../middlewares/auth.js";
-import dotenv from "dotenv";
 
 const router = express.Router();
 
-dotenv.config({
-  path: "./config/config.env",
-});
 router.get(
   "/google",
   passport.authenticate("google", {
     scope: ["profile"],
   })
 );
-const frontend = process.env.FRONTEND_URL;
 
 router.get(
   "/login",
   passport.authenticate("google", {
-    successRedirect: frontend,
-    scope: ["profile"],
+    successRedirect: process.env.FRONTEND_URL,
   })
 );
 router.get("/me", isAuthenticated, myProfile);
